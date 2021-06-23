d211de4d8b209d3d32c38334064a0d07de10fb21
alexey-lapin <alexey-lapin@protonmail.com>
2021-06-23 19:15:14 +0000

    Deploying to gh-pages from @ alexey-lapin/spring-boot-startup-analyzer@aa214a6bbe1a8a980df0f04433b5cb373317164a 🚀

---
+++ js/about.cc124d59.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),u={class:"about"},c=Object(e["k"])("h1",null,"This is an about page",-1);function o(n,t){return Object(e["u"])(),Object(e["g"])("div",u,[c])}const s={};s.render=o;t["default"]=s}}]);
+//# sourceMappingURL=about.cc124d59.js.map
