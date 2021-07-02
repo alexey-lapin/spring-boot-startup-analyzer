9d341db4197a79fa024832ebcc5e446f9341a60c
Alexey Lapin <alexey-lapin@protonmail.com>
2021-07-02 22:07:31 +0300

    improve usage

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -12,11 +12,31 @@
   >
   <Panel header="Spring configuration" class="p-mb-2">
     Configure your application to collect startup events:
-    <CodeBlock :code="codeJava" language="java" class="p-mt-2" />
+    <CodeBlock :code="codeJavaApp" language="java" class="p-mt-2" />
   </Panel>
   <Panel header="To Analyze URL" class="p-mb-2">
     Configure your application to handle requests from this web page (CORS)
     <CodeBlock :code="codeProperties" language="properties" class="p-mt-2" />
+    Configure security (if it used in your application) according to
+    <a
+      href="https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html#actuator.endpoints.security"
+      >docs</a
+    >
+    <CodeBlock :code="codeJavaApp" language="java" class="p-mt-2" />
+  </Panel>
+  <Panel header="To Analyze URL" class="p-mb-2">
+    Configure your application to handle requests from this web page (CORS)
+    <CodeBlock
+      :code="codeProperties"
+      language="properties"
+      class="p-mt-2 p-mb-2"
+    />
+    Configure security (if it used in your application) according to
+    <a
+      href="https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html#actuator.endpoints.security"
+      >docs</a
+    >
+    <CodeBlock :code="codeJavaSecurity" language="java" class="p-mt-2" />
   </Panel>
   <Panel header="To Analyze File" class="p-mb-2">
     Save json response from <b>/actuator/startup</b> endpoint to file and use it
@@ -44,13 +64,24 @@ import CodeBlock from "@/components/CodeBlock.vue";
   },
 })
 export default class Analyzer extends Vue {
-  codeJava = dedent(`
+  codeJavaApp = dedent(`
     @SpringBootApplication
     public class App {
         public static void main(String[] args) {
             SpringApplication application = new SpringApplication(App.class);
             application.setApplicationStartup(new BufferingApplicationStartup(1000));
             application.run(args);
+        }
+    }
+  `);
+  codeJavaSecurity = dedent(`
+    @Configuration(proxyBeanMethods = false)
+    public class ActuatorSecurityConfiguration {
+        @Bean
+        public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+            http.requestMatcher(EndpointRequest.toAnyEndpoint())
+                    .authorizeRequests((requests) -> requests.anyRequest().permitAll());
+            return http.build();
         }
     }
   `);
