ed7b3376881bdae256cedeae043d735077496c90
Alexey Lapin <alexey-lapin@protonmail.com>
2023-10-08 14:42:55 +0200

    fix decimal numbers

--- src/components/AnalysisTable.vue
+++ src/components/AnalysisTable.vue
@@ -1,16 +1,20 @@
 <template>
   <DataTable :value="eventsStore.events" tableStyle="table-layout: fixed;">
-    <Column field="id" header="Id" headerClass="w-1" :sortable="true"></Column>
-    <Column field="parentId" header="PId" headerClass="w-1" :sortable="true"></Column>
-    <Column field="name" header="Name" :sortable="true"></Column>
-    <Column field="summary" header="Sum" headerClass="w-1" :sortable="true">
+    <Column field="id" header="Id" headerClass="w-6rem" :sortable="true"></Column>
+    <Column field="parentId" header="PId" headerClass="w-6rem" :sortable="true"></Column>
+    <Column field="name" header="Name" headerClass="w-20rem" :sortable="true"></Column>
+    <Column field="summary" header="Sum" headerClass="w-6rem" :sortable="true">
       <template #body="slotProps">
-        <span>{{ slotProps.data.summary.toLocaleString(undefined, {maximumFractionDigits: 3}) }}</span>
+        <span>{{
+          slotProps.data.summary.toLocaleString(undefined, { maximumFractionDigits: 3 })
+        }}</span>
       </template>
     </Column>
-    <Column field="duration" header="Dur" headerClass="w-1" :sortable="true">
+    <Column field="duration" header="Dur" headerClass="w-6rem" :sortable="true">
       <template #body="slotProps">
-        <span>{{ slotProps.data.duration.toLocaleString(undefined, {maximumFractionDigits: 3}) }}</span>
+        <span>{{
+          slotProps.data.duration.toLocaleString(undefined, { maximumFractionDigits: 3 })
+        }}</span>
       </template>
     </Column>
     <Column header="Tags">
