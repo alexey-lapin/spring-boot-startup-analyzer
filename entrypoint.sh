62956abd10b5be3dc52cf1dc260421685e245579
Christian Gintenreiter <christian.gintenreiter@smarter-ecommerce.com>
2024-01-19 13:28:48 +0100

    Fix multi-file selection in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 : "${UI_PUBLIC_PATH:=}"
 
 sed -i "s@/<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/index.html
-sed -i "s@/<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/assets/index.*.js
-sed -i "s@/<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/assets/index.*.css
+sed -i "s@/<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/assets/index*.js
+sed -i "s@/<PUBLIC_PATH_REPLACE>@$UI_PUBLIC_PATH@g" /usr/share/nginx/html/assets/index*.css
