14d20426664a4b2be6fc25a1cece99860d16d051
Alexey Lapin <alexey-lapin@protonmail.com>
2025-11-11 23:07:57 -0500

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_APP_PUBLIC_PATH ?? "/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
